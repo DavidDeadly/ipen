a73bc4a3734365b56239943383b8564da3c88d2b
DavidDeadly <jdrueda513@gmail.com>
2024-11-29 20:18:32 -0500

    refactor: create app unit

---
+++ src/app.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2024 DavidDeadly
+#pragma once
+
+#include "GLFW/glfw3.h"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkImage.h"
+#include "include/core/SkSurface.h"
+#include "include/gpu/ganesh/GrDirectContext.h"
+#include "include/gpu/ganesh/gl/GrGLDirectContext.h"
+
+class App {
+public:
+  std::string name;
+  int width;
+  int height;
+
+private:
+  SkSurface *surface;
+  GrDirectContext *context;
+  GLFWwindow *window;
+
+public:
+  // Initializes GLFW and SKIA
+  App(std::string name, int width, int height);
+
+  // Destructor to clean up resources used for GLFW and SKIA
+  ~App();
+
+  void start();
+
+private:
+  void init_skia(int w, int h);
+};
